158b818c3c5a95b7c07ba4ef042727077b1f5705
Martin Andreani <andreani.martin4@gmail.com>
2025-11-22 17:35:49 -0300

    fix

--- app/db/alembic/versions/d9fee4b33ec3_insert_datos.py
+++ app/db/alembic/versions/d9fee4b33ec3_insert_datos.py
@@ -20,9 +20,38 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    pass
-
+    #Roles
+    op.execute(
+        """
+        INSERT INTO roles (nombre, descripcion, activo)
+        VALUES 
+        ('ADMIN', 'Administrador del sistema', true),
+        ('MEDICO', 'Medico del sistema', true),
+        ('RECEPCIONISTA', 'Recepcionista del sistema', true),
+        ('PACIENTE', 'Paciente del sistema', true);
+        """
+    )
+    
+    
+    #Estados Turnos
+    op.execute(
+        """
+        INSERT INTO estados_turnos (nombre, descripcion, activo)
+        VALUES
+        ('AGENDADO', 'El paciente lo elige y se crea el registro en Turnos.', true),
+        ('PENDIENTE', 'Turno pendiente de confirmacion', true),
+        ('CONFIRMADO', 'El paciente confirma asistencia al turno.', true),
+        ('EN SALA DE ESPERA', 'El paciente se encuentra en la sala de espera del consultorio.', true),
+        ('EN ATENCION', 'El paciente se encuentra siendo atendido por el medico.', true),
+        ('ATENDIDO', 'El paciente ha sido atendido y el turno finalizo correctamente.', true),
+        ('AUSENTE', 'El paciente no se presento al turno.', true),
+        ('CANCELADO POR PACIENTE', 'El paciente cancelo el turno.', true),
+        ('CANCELADO POR MEDICO', 'El medico cancelo el turno.', true);
+        """
+    )
+    
 
 def downgrade() -> None:
     """Downgrade schema."""
-    pass
+    op.execute("DELETE FROM roles;")
+    op.execute("DELETE FROM estados_turnos;")
